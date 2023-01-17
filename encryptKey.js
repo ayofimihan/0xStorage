b331262078f0ac05faf11bfa358483c370079b8e
ayofimihan <cogunsina@gmail.com>
2023-01-17 09:28:41 +0100

    added encryptkey

--- encryptKey.js
+++ encryptKey.js
@@ -2,7 +2,15 @@ const ethers = require("ethers");
 const fs = require("fs");
 require("dotenv").config(); 
 
-async function main() {}
+async function main() {
+    const privateKey = process.env.PRIVATE_KEY;
+    const password = process.env.PASSWORD;
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+    const encryptedJsonKey = await wallet.encrypt(password,privateKey)
+    console.log(encryptedJsonKey);
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
+
+}
 main()
   .then(() => process.exitCode(0))
   .catch((error) => {
